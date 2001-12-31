49f9254eb77219dd8ffb34e2fcdf687b8e4a6511
nuke2015 <196971567@qq.com>
2002-01-01 00:03:12 +0800

    去掉多余的服务

--- win/services/大杀招.bat
+++ win/services/大杀招.bat
@@ -1,4 +1,20 @@
 
-rem 专杀会自动从禁用状态复活的服务
+rem 会自动复活
 sc delete CryptSvc
+
+rem 去掉没啥用
+sc delete WinHttpAutoProxySvc
+sc delete FontCache
+sc delete CscService
+sc delete IKEEXT
+sc delete iphlpsvc
+sc delete AeLookupSvc
+sc delete BFE
+sc delete DPS
+sc delete WdiServiceHost
+sc delete WdiSystemHost
+sc delete TrkWks
+sc delete PcaSvc
+sc delete AMD External Events Utility
+sc delete KeyIso
 
