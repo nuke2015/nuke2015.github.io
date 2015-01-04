87c4225899cea9bfbaff4a6612b649e573076803
nuke2015 <nuke.zou@corp.to8to.com>
2015-01-04 14:32:40 +0800

    sds

---
+++ 常用碎片/lotus.php
@@ -0,0 +1,11 @@
+<?php
+
+//lotus赋值
+$this->data = $return;
+//lotus模板解析
+$this->responseType = 'tpl';
+//lotus布局
+$this->layout = 'index';
+//lotus模板文件名
+$this->template='log-index';
+
--- 常用碎片/upload.php
+++ 常用碎片/upload.php
@@ -1,5 +1,22 @@
 <?php
 
+
+//获取上传的图片并返回图片路径 注：该上传代码放在开头，否则在360极速模式下无法上传
+if (!empty($_FILES)) {
+    $targetFolder = '/huodong/day_' . date('ymd');
+    $tempFile = $_FILES['Filedata']['name'];
+    $targetPath = ROOT. '/pic/' . $targetFolder;
+    $targetPath = mkdirs_fun($targetPath);
+    $type = strtolower(filekzm($_FILES["Filedata"]["name"]));
+    $allowTypes=array('.png','.jpg','.gif');
+    if(!in_array($type,$allowTypes))jsonTip(0,'文件类型不允许!');
+    $img_url = '/' . date('Ymd') . '_' . md5($_FILES['Filedata']['name'] . time()) . $type;
+    $targetFile = $targetPath . '/' . date('Ymd') . '_' . md5($_FILES['Filedata']['name'] . time()) . $type;
+    $isOk = move_uploaded_file($_FILES['Filedata']['tmp_name'], $targetFile);
+    $img_url = PIC_DOMAIN . $targetFolder . $img_url;
+    jsonTip(1,$img_url);
+}
+
 checkSess();
 
 //消息提示;
