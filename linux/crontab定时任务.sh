65c0ab1c8c572caab9540314d39968833183d25a
nuke2015 <nuke.zou@corp.to8to.com>
2015-04-11 10:58:02 +0800

    2015年4月11日 10:58:00

---
+++ linux/crontab定时任务.sh
@@ -0,0 +1,17 @@
+
+#启用服务
+service crond start
+
+#编辑任务
+crontab -e
+
+# vi输入,分分钟执行一次/home/test.sh把结果输出到/home/xxx.log
+*/1  *  *  *  *  root  /home/test.sh >> /home/xxx.log
+然后保存:wq
+
+#查看任务列表
+crontab -l
+
+cd /home
+ls
+
