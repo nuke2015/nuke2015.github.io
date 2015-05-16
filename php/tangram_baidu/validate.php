f5413898784338ba1008c687f6e0b8b03e2564fa
nuke2015 <196971567@qq.com>
2015-05-16 18:06:28 +0800

    js表单验证

---
+++ php/tangram_baidu/validate.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+        <script type="text/javascript" src="http://fe.bdimg.com/tangram/2.0.2.5.js"></script>
+    </head>
+    <body>
+    <form action="#" style="margin:100px">
+        <p>请填写Email地址</p>
+        <label for="kama">#mail:</label>
+        <input type="email" required="required"/>
+        </br>
+        <button type="submit">提交</button>
+    </form>
+    <script>
+        T("form").validator(); 
+    </script>
+    </body>
+</html>
