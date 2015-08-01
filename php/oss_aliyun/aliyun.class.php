b296c42f4b794aaec94393622ba29cb6af5b0e02
锋子 <admin@bestphp.net>
2015-08-01 12:17:06 +0800

    增加切片上传方法

--- php/oss_aliyun/aliyun.class.php
+++ php/oss_aliyun/aliyun.class.php
@@ -1,20 +1,30 @@
 <?php
 
+// 测试
+// require_once APP_PATH . 'Lib/ORG/oss_php_sdk/aliyun.class.php';
+// $video_path = ROOT_PATH . '/Ftp/20150731/kuguachaomuerA.mp4';
+// $file_save_name = 'test_part_20150801.mp4';
+// $resp = aliyun::upload_by_multi_part_video($video_path, $file_save_name);
+// print_r($resp);
+// echo 'done';
+
+
+
 /**
  * 加载sdk包以及错误代码包
  */
 require_once __DIR__ . '/sdk.class.php';
-// 在conf.inc.php文件里写配置
 
 class aliyun
 {
-    static $bucket_image = 'zhangchu-image';
-    static $bucket_video = 'zhangchu-video';
+    static $bucket_image = 'bestphp-image';
+    static $bucket_video = 'bestphp-video';
+    static $aliyun_node = 'oss-cn-qingdao.aliyuncs.com';
     
     public static function init() {
         
         // 初始化
-        $oss_sdk_service = new ALIOSS(OSS_ACCESS_ID, OSS_ACCESS_KEY, 'oss-cn-shenzhen.aliyuncs.com');
+        $oss_sdk_service = new ALIOSS(OSS_ACCESS_ID, OSS_ACCESS_KEY, self::$aliyun_node);
         
         //设置是否打开curl调试模式
         $oss_sdk_service->set_debug_mode(true);
@@ -38,5 +48,17 @@ class aliyun
     public static function upload_file_by_file($file_path, $object) {
         $oss_sdk_service = self::init();
         return $oss_sdk_service->upload_file_by_file(self::$bucket_image, $object, $file_path);
+    }
+    
+    /**
+     * 切片上传
+     * @param  [type] $file_path [description]
+     * @param  [type] $object    [description]
+     * @return [type]            [description]
+     */
+    public static function upload_by_multi_part_video($file_path, $object) {
+        $oss_sdk_service = self::init();
+        $options = array(ALIOSS::OSS_FILE_UPLOAD => $file_path, 'partSize' => 5242880,);
+        return $oss_sdk_service->create_mpu_object(self::$bucket_video, $object, $options);
     }
 }
