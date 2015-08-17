3b2d721d9fb9a5769f1e8a8f4aca678bb95776aa
锋子 <admin@bestphp.net>
2015-08-17 19:04:11 +0800

    大文本切片函数.用来导日志时.分段切片处理.

--- 常用碎片/files文件处理类.class.php
+++ 常用碎片/files文件处理类.class.php
@@ -32,7 +32,8 @@ class files
             foreach ($list as $value) {
                 if (is_dir($value)) {
                     $result = array_merge($result, self::files_tree($value, $file_extension));
-                } else {
+                } 
+                else {
                     
                     //目标类型过滤;
                     if ($file_extension && (self::file_ext($value) != $file_extension)) continue;
@@ -71,5 +72,29 @@ class files
         $extension = pathinfo($filename, PATHINFO_EXTENSION);
         $extension = strtolower('.' . $extension);
         return $extension;
+    }
+    
+    /**
+     * 大文本分片读取
+     * 作用类似于,数组切片.
+     * 注意释放资源,不用file($filename)数组;
+     * 此处直接返回单行文本内容不拆分,
+     * 而在业务消费处把文本拆分成数组,
+     * 有利于当前函数功能复用.
+     * @param  [type] $i [description]
+     * @return [type]    [description]
+     */
+    public static function slice($filename, $offset, $length) {
+        $result = array();
+        $fp = fopen($filename, "r");
+        while (!feof($fp)) {
+            $i++;
+            $line = fgets($fp);
+            if ($i < $offset) continue;
+            $result[] = trim($line);
+            if ($i >= $offset + $length) break;
+        }
+        fclose($fp);
+        return $result;
     }
 }
