4ade6e024da6f26e1fcd39c6ac2460c298e6c1df
nuke2015 <196971567@qq.com>
2015-10-04 03:43:14 +0800

    右键打开_格式关联修正版

---
+++ 常用碎片/其它/右键打开gvim_格式关联.bat
@@ -0,0 +1,56 @@
+@echo off
+echo FileExts setting batch with sublime
+rem http://github.com/nuke2015
+set tmp_reg=%temp%\tmp.ini
+set Application=gvim.exe
+set Application_command=D:\\working\\Gvim\\vim74\\gvim.exe
+set FileExts=.ini .txt .cnf .conf .css .log .cfg .xml .php .asp .java .js .css .htm .sh .py .go
+
+
+rem do it now!
+echo %Application%
+echo %FileExts%
+del %tmp_reg% > nul
+
+rem  Application setting;
+
+echo Windows Registry Editor Version 5.00>%tmp_reg%
+echo.>>%tmp_reg%
+echo [HKEY_CLASSES_ROOT\*\shell\%Application%]>>%tmp_reg%
+echo @="ÓÃSublime´ò¿ª">>%tmp_reg%
+echo [HKEY_CLASSES_ROOT\*\shell\%Application%\command]>>%tmp_reg%
+echo @="%Application_command% %1">>%tmp_reg%
+echo.>>%tmp_reg%
+echo [HKEY_CLASSES_ROOT\Folder\shell\%Application%]>>%tmp_reg%
+echo @="Sublime">>%tmp_reg%
+echo [HKEY_CLASSES_ROOT\Folder\shell\%Application%\command]>>%tmp_reg%
+echo @="%Application_command% %1">>%tmp_reg%
+echo.>>%tmp_reg%
+echo [HKEY_CURRENT_USER\Software\Classes\Applications\%Application%]>>%tmp_reg%
+echo [HKEY_CURRENT_USER\Software\Classes\Applications\%Application%\shell\open\command]>>%tmp_reg%
+echo @="%Application_command% %1">>%tmp_reg%
+
+rem FileExts setting;
+for %%i in (%FileExts%) do (
+	echo [-HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\%%i]>>%tmp_reg%
+	echo.>>%tmp_reg%
+	echo [HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\%%i]>>%tmp_reg%
+	echo "Application"="Applications\\%Application%">>%tmp_reg%
+	echo.>>%tmp_reg%
+	echo [HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\%%i\OpenWithList]>>%tmp_reg%
+	echo "a"="notepad.exe">>%tmp_reg%
+	echo "b"="gvim.exe">>%tmp_reg%
+	echo "c"="notepad++.exe">>%tmp_reg%
+	echo "d"="%Application%">>%tmp_reg%
+	echo "MRUList"="dcba">>%tmp_reg%
+	echo.>>%tmp_reg%
+	echo [HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\%%i\UserChoice]>>%tmp_reg%
+	echo "Progid"="Applications\\%Application%">>%tmp_reg%
+	echo.>>%tmp_reg%
+)
+
+regedit /s %tmp_reg%
+taskkill /im explorer.exe /f > nul
+start C:\Windows\explorer.exe
+echo done.
+pause
