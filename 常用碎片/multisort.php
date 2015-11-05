e8f77a39f89d7e9b1e37ef58fba5826c2943e2fc
锋子 <admin@bestphp.net>
2015-11-05 11:28:12 +0800

    二维排序.

---
+++ 常用碎片/multisort.php
@@ -0,0 +1,13 @@
+<?php
+
+// 二维排序
+public static function multisorter($data, $fields) {
+    $sorter = array();
+    if ($data && count($data)) {
+        foreach ($data as $key => $value) {
+            $sorter[] = $value[$field];
+        }
+        array_multisort($sorter, SORT_ASC, $data);
+    }
+    return $data;
+}
