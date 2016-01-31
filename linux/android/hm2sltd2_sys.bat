9b7eaf3ecfd07860dac9d8a42d2c3fc4be615b27
nuke2015 <196971567@qq.com>
2016-02-01 02:04:12 +0800

    擦掉再写入,很快.

--- linux/android/hm2sltd2_sys.bat
+++ linux/android/hm2sltd2_sys.bat
@@ -1,6 +1,7 @@
 echo "Fastboot Tool Ver 7.0"
 
-
+rem erase and send and write is faster more!
+fastboot %* erase boot
 
 fastboot %* flash boot %~dp0images\boot.img
 fastboot %* flash cache %~dp0images\cache.img
