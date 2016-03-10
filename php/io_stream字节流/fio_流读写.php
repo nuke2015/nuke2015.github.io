fdc5889ee00f6b3d328c1cedf9452b94743eb301
nuke2015 <admin@bestphp.net>
2016-03-10 11:25:16 +0800

    增加一个字节流读写助手

---
+++ php/io_stream字节流/fio_流读写.php
@@ -0,0 +1,27 @@
+<?php
+
+$url  = 'http://video.szzhangchu.com/gaogainaiyansudabingganA.mp4';
+$file = "test.mp4";
+fio::stream_to_stream($url, $file);
+
+echo 'done!';
+
+class Fio
+{
+
+    // 外封装
+    public static function stream_to_stream($url, $file)
+    {
+        self::pipe_streams(fopen($url, 'r'), fopen($file, 'w'));
+    }
+
+    // io流
+    public static function pipe_streams($in, $out)
+    {
+        $size = 0;
+        while (!feof($in)) {
+            $size += fwrite($out, fread($in, 8192));
+        }
+        return $size;
+    }
+}
