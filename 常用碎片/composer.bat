2410beb6bd3f442e9f77b7e51b24084a85b933f1
nuke2015 <196971567@qq.com>
2016-03-18 18:12:07 +0800

    composer

--- 常用碎片/composer.bat
+++ 常用碎片/composer.bat
@@ -1,3 +1,3 @@
 rem curl -sS https://getcomposer.org/installer | php
-php -r "readfile('https://getcomposer.org/installer');" | php
+rem php -r "readfile('https://getcomposer.org/installer');" | php
 @php "%~dp0composer.phar" %*
