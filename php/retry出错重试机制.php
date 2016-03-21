11bab18d28908d24f61120fb260676fa5d67b519
nuke2015 <196971567@qq.com>
2016-03-21 18:14:50 +0800

    重试机制算法改良，把业务逻辑单独提取出云。

--- php/retry出错重试机制.php
+++ php/retry出错重试机制.php
@@ -2,27 +2,37 @@
 
 // 对于部分远程调用失败以后,只重试一次!
 
-test::hello();
+test::main();
 
-class test {
+class test
+{
     private static $retry = 0;
-    public static function hello() {
-        echo '<br/>';
+
+    // 子调用
+    private static function hello()
+    {
+        // 模拟环境干扰
         $rand = rand(0, 1);
-        self::$retry+= 1;
         if ($rand) {
-            echo 'hello';
+            echo 'hello<br/>';
+            return true;
         } else {
-            echo 'error';
-            
-            //出错才重试一次;
-            if (self::$retry == 1) {
-                self::hello();
+            echo 'error<br/>';
+            return false;
+        }
+    }
+
+    public static function main()
+    {
+        while (!$status = self::hello()) {
+            self::$retry += 1;
+            if (self::$retry > 10) {
+                break;
             }
+
         }
     }
 }
-
 
 // 正常结果:
 // hello
