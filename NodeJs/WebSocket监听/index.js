326b0f032839f9bf7d91f5eded3f540987b40e74
锋子 <admin@bestphp.net>
2016-08-10 17:32:51 +0800

    websocket监听

---
+++ NodeJs/WebSocket监听/index.js
@@ -0,0 +1,22 @@
+var ws = require("nodejs-websocket");
+console.log("开始建立连接...")
+console.log("0.0.0.0:8001")
+var game1 = null,
+    game2 = null,
+    game1Ready = false,
+    game2Ready = false;
+var myDate=new Date();
+var server = ws.createServer(function(conn) {
+    conn.on("text", function(str) {
+        console.log("rec:"+myDate.toLocaleString());
+        console.log(str);
+        conn.sendText(str);
+    });
+    conn.on("close", function(code, reason) {
+        console.log("关闭连接")
+    });
+    conn.on("error", function(code, reason) {
+        console.log("异常关闭")
+    });
+}).listen(8001);
+console.log("WebSocket listening>>");
