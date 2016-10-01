00f181c179b2f0bf83324526df98504fc4037d15
锋子 <admin@bestphp.net>
2016-10-01 12:48:34 +0800

    节点查询

--- php/采集/QueryList_demo.php
+++ php/采集/QueryList_demo.php
@@ -1,31 +1,21 @@
 <?php
 
 //采集某页面html内容
-$data = QueryList::Query('http://cms.querylist.cc/bizhi/453.html', array(
-    'html' => array(),
-));
+$data = QueryList::Query('http://cms.querylist.cc/bizhi/453.html', array('html' => array(),));
+
 //打印结果
 print_r($data);
 exit;
 
 //采集某页面所有的图片
-$data = QueryList::Query('http://cms.querylist.cc/bizhi/453.html', array(
-    'images' => array('img', 'src'),
-));
+$data = QueryList::Query('http://cms.querylist.cc/bizhi/453.html', array('images' => array('img', 'src'),));
+
 //打印结果
 print_r($data);
 exit;
 
 //使用插件,定制协议头,定制采集结果
-$urls = QueryList::run('Request', array(
-    'target'     => 'http://cms.querylist.cc/news/list_2.html',
-    'referrer'   => 'http://cms.querylist.cc',
-    'method'     => 'GET',
-    'params'     => ['var1' => 'testvalue', 'var2' => 'somevalue'],
-    'user_agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0',
-    'cookiePath' => './cookie.txt',
-    'timeout'    => '30',
-))->setQuery(['link' => array('h2>a', 'href'), 'html' => array('*', 'html')])->getData();
+$urls = QueryList::run('Request', array('target' => 'http://cms.querylist.cc/news/list_2.html', 'referrer' => 'http://cms.querylist.cc', 'method' => 'GET', 'params' => ['var1' => 'testvalue', 'var2' => 'somevalue'], 'user_agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0', 'cookiePath' => './cookie.txt', 'timeout' => '30',))->setQuery(['link' => array('h2>a', 'href'), 'html' => array('*', 'html') ])->getData();
 print_r($urls);
 exit;
 
@@ -33,20 +23,39 @@ exit;
 $i = 0;
 
 //使用插件
-$urls = QueryList::run('Request', array(
-    'target'     => 'http://cms.querylist.cc/news/list_2.html',
-    'referrer'   => 'http://cms.querylist.cc',
-    'method'     => 'GET',
-    'params'     => ['var1' => 'testvalue', 'var2' => 'somevalue'],
-    'user_agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0',
-    'cookiePath' => './cookie.txt',
-    'timeout'    => '30',
-))->setQuery(['link' => array('h2>a', 'href'), 'html' => array('h2>a', 'text', '', function ($txt) {
+$urls = QueryList::run('Request', array('target' => 'http://cms.querylist.cc/news/list_2.html', 'referrer' => 'http://cms.querylist.cc', 'method' => 'GET', 'params' => ['var1' => 'testvalue', 'var2' => 'somevalue'], 'user_agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0', 'cookiePath' => './cookie.txt', 'timeout' => '30',))->setQuery(['link' => array('h2>a', 'href'), 'html' => array('h2>a', 'text', '', function ($txt) {
     return 'hello' . $txt;
-})])->getData(function ($item) {
+}) ])->getData(function ($item) {
     global $i;
     $item['index'] = $i++;
     return $item;
 });
+print_r($urls);
+exit;
+
+//只查询不采集
+$urls = QueryList::Query('<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td><a href="#">链接</a></td>
+                <td><a href="#">链接</a></td>
+                <td><a href="#">链接</a></td>
+                <td><a href="#">链接</a></td>
+            </tr>
+            <tr>
+                <td><a href="#">链接</a></td>
+                <td><a href="#">链接</a></td>
+                <td><a href="#">链接</a></td>
+                <td><a href="#">链接</a></td>
+            </tr>
+        </table>
+    </body>
+</html>', ['link' => array('a', 'href'), 'text' => array('a', 'text') ])->getData();
 print_r($urls);
 exit;
