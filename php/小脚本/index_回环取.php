1ab25fa9913013dd9f7167f2c3d41ceac5e59d99
锋子 <admin@bestphp.net>
2016-10-02 22:33:38 +0800

    磨人的小妖精

---
+++ php/小脚本/index_回环取.php
@@ -0,0 +1,72 @@
+<?php
+
+$a = range(1, 10);
+
+// 回环取
+function array_circle($arr, $pos, $len)
+{
+    $total = count($arr);
+    if ($pos + $len < $total && $pos + $len > $len) {
+        $result = array_slice($arr, $pos, $len);
+    } elseif ($pos + $len >= $total) {
+        $result_a = array_slice($arr, $pos, $len);
+        $pos + $len - $total;
+        $result_b = array_slice($arr, 0, $pos + $len - $total);
+        $result   = array_merge($result_a, $result_b);
+    } elseif ($pos + $len < $len) {
+        $result_a = array_slice($arr, $pos, $len);
+        $result_b = array_slice($arr, 0, $pos + $len);
+        $result   = array_merge($result_a, $result_b);
+    }
+    return $result;
+}
+
+print_r($a);
+print_r(array_circle($a, 2, 4));
+print_r(array_circle($a, 8, 4));
+print_r(array_circle($a, 10, 4));
+print_r(array_circle($a, -2, 4));
+
+
+
+// Array
+// (
+//     [0] => 1
+//     [1] => 2
+//     [2] => 3
+//     [3] => 4
+//     [4] => 5
+//     [5] => 6
+//     [6] => 7
+//     [7] => 8
+//     [8] => 9
+//     [9] => 10
+// )
+// Array
+// (
+//     [0] => 3
+//     [1] => 4
+//     [2] => 5
+//     [3] => 6
+// )
+// Array
+// (
+//     [0] => 9
+//     [1] => 10
+//     [2] => 1
+//     [3] => 2
+// )
+// Array
+// (
+//     [0] => 1
+//     [1] => 2
+//     [2] => 3
+//     [3] => 4
+// )
+// Array
+// (
+//     [0] => 9
+//     [1] => 10
+//     [2] => 1
+//     [3] => 2
+// )
