34acdf0f6dc20ff39d06816f371d09ae8a054d38
锋子 <admin@bestphp.net>
2016-12-09 10:08:41 +0800

    成功入库啦

--- NodeJs/jugglingdb-mysql/doc/create.js
+++ NodeJs/jugglingdb-mysql/doc/create.js
@@ -5,12 +5,25 @@ var db = new Schema('mysql', {
     database: 'test',
     username: 'root'
 });
+// 清除数据库
+// 一个指向就够了,不用详细定义
 var model = db.define('hello');
 console.log(model);
-model.create({
-    title: 'dsfn',
-    time: '2016-12-01 16:01:02',
-}, function(err, res) {
+model.destroyAll(function(err) {
     console.log(err);
-    console.log(res);
 });
+// 入库一批数据
+// 这是代码端的数据定义,若不定义则入库为空
+var model = db.define('hello', {
+    title: Schema.Text,
+    time: Schema.Text
+});
+for (var i = 5; i >= 0; i--) {
+    model.create({
+        title: 'test ' + i,
+        time: new Date().toLocaleString(),
+    }, function(err, res) {
+        console.log(err);
+        console.log(res);
+    });
+}
