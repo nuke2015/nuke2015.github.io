fe1504dd968617cd1c21def8b8d5f6686344db48
锋子 <admin@bestphp.net>
2017-04-05 16:07:03 +0800

    增加多个上传.

--- php/oss_html/debug_upload2_原生传送.js
+++ php/oss_html/debug_upload2_原生传送.js
@@ -1,31 +1,42 @@
-// 原生的aliyun oss传图片的方法
-var oss_host = 'http://m.t.jjys168.com/oss.php';
-var filename_to = Math.random();
-$("#img_input").on("change", function(e) {
-    var file = e.target.files[0]; //获取图片资源
-    // 只选择图片文件
-    if (!file.type.match('image.*')) {
-        return false;
+var oss_uper = {
+    oss_host: 'http://m.t.jjys168.com/oss.php',
+    oss_host_to: 'http://upload.jjys168.com/html5_temp/',
+    send: function(file_loc, cb) {
+        console.log('oss_uper', filename_to, file_loc);
+        var filename_to = this.random();
+        // 先请求授权，然后回调
+        $.getJSON(this.oss_host, function(json) {
+            var ossData = new FormData();
+            var oReq = new XMLHttpRequest();
+            //签名用的PHP
+            // 添加签名信息
+            ossData.append('OSSAccessKeyId', json.accessid);
+            ossData.append('policy', json.policy);
+            ossData.append('signature', json.signature);
+            ossData.append('key', json.dir + filename_to);
+            // 添加文件
+            ossData.append('file', file_loc);
+            oReq.onreadystatechange = cb(filename_to);
+            oReq.open("POST", json.host);
+            oReq.send(ossData);
+        });
+    },
+    random: function() {
+        return Math.random();
     }
-    console.log(file);
-    var ossData = new FormData();
-    var oReq = new XMLHttpRequest();
-    // 先请求授权，然后回调
-    $.getJSON(oss_host, function(json) {
-        //签名用的PHP
-        // 添加签名信息
-        ossData.append('OSSAccessKeyId', json.accessid);
-        ossData.append('policy', json.policy);
-        ossData.append('signature', json.signature);
-        ossData.append('key', json.dir + filename_to);
-        // 添加文件
-        ossData.append('file', file);
-        oReq.onreadystatechange = function(res) {
-            // 这里是回调函数
-            console.log(res);
-            console.log(filename_to);
-        };
-        oReq.open("POST", json.host);
-        oReq.send(ossData);
-    });
+};
+$("#selectfiles").on("change", function(e) {
+    $('#preview_box').html();
+    for (i = 0; i < e.target.files.length; i++) {
+        var file = e.target.files[i];
+        if (file.size > 1024000) {
+            console.log('file.size>', file.size);
+            return false;
+        }
+        var uper = oss_uper;
+        uper.send(file, function(res) {
+            var html = '<img src="' + uper.oss_host_to + res + '" class="oss_file" ></img>';
+            $('#preview_box').append(html);
+        });
+    }
 });
