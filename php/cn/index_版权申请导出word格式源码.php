800fa9b1993a8f61990bece7cbaf2352c4b0737b
锋子 <admin@bestphp.net>
2017-08-09 19:18:18 +0800

    版权申请,导出60页代码.

---
+++ php/cn/index_版权申请导出word格式源码.php
@@ -0,0 +1,52 @@
+<?php
+
+$files = dir_list('./x/');
+
+$leng   = 30;
+$i      = 0;
+$result = '';
+if ($files && count($files)) {
+    foreach ($files as $key => $value) {
+        if (stripos($value, '.php') && stripos($value, 'Conf') !== -1) {
+            $result .= file_get_contents($value);
+            $i++;
+            if ($i >= $leng) {
+                break;
+            }
+
+        }
+    }
+}
+Header("Content-type:application/vnd.ms-word");
+Header("Content-Disposition: attachment;filename=didiyuesao.doc");
+echo $result;
+exit;
+
+function dir_list($path, $exts = '', $list = array())
+{
+    $path  = dir_path($path);
+    $files = glob($path . '*');
+    foreach ($files as $v) {
+        $fileext = fileext($v);
+        if (!$exts || preg_match("/\.($exts)/i", $v)) {
+            $list[] = $v;
+            if (is_dir($v)) {
+                $list = dir_list($v, $exts, $list);
+            }
+        }
+    }
+    return $list;
+}
+function dir_path($path)
+{
+    $path = str_replace('\\', '/', $path);
+    if (substr($path, -1) != '/') {
+        $path = $path . '/';
+    }
+
+    return $path;
+}
+function fileext($filename)
+{
+    return strtolower(trim(substr(strrchr($filename, '.'), 1, 10)));
+}
