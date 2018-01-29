ad000a899d047abd8b49432fcd577dcc9b96c757
锋子 <admin@bestphp.net>
2018-01-29 16:17:34 +0800

    增加方法 反射.

--- php/thinkphp/class_view_类反射.php
+++ php/thinkphp/class_view_类反射.php
@@ -7,4 +7,12 @@ $ddys_banner = new model\ddys_banner();
 $r           = new ReflectionClass($ddys_banner);
 var_dump($r->getConstants(), "Constants");
 var_dump($r->getProperties(), "Properties");
-var_dump($r->getMethods(), "Methods");
+
+$ary = $r->getMethods();
+var_dump($ary, "methods");
+
+foreach ($ary as $key => $method) {
+    $classMethod   = new ReflectionMethod($ddys_banner, $method->name);
+    $argumentCount = count($classMethod->getParameters());
+    var_dump($method->name . " args_count:" . $argumentCount);
+}
