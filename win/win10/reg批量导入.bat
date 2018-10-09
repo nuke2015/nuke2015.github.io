acd9fb74bfb36866f03974ab7d556d5b6e3632e7
锋子 <admin@bestphp.net>
2018-10-09 10:20:02 +0800

    修复.

--- win/win10/reg批量导入.bat
+++ win/win10/reg批量导入.bat
@@ -1,7 +1,5 @@
-@echo off
-echo.
-set root_path=%cd%
-for /f %%i in ('dir /a /b %root_path%\reg\*.reg') do regedit /s %root_path%\reg\%%i
-echo 注册表文件导入完成按任意键退出...
-pause>nul
-exit
+
+for %%1 in (%cd%\reg\*.reg) do regedit /s %%1
+
+
+pause
