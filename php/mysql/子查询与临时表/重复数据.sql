1f35578996da4465d2f6630a4e6d27ab647638d2
fengfeng <it07@qq.com>
2019-08-20 13:49:38 +0800

    +

--- php/mysql/子查询与临时表/重复数据.sql
+++ php/mysql/子查询与临时表/重复数据.sql
@@ -11,3 +11,23 @@ select id,group_id,member_id,club_id from zhihu_group_member where member_id in
 )
 
 
+
+
+
+-- 删除重复
+
+delete from zhihu_rbac_role_member
+where id in 
+(
+        
+    -- 重复
+    select id from zhihu_rbac_role_member where concat(member_id,club_id) in
+    (
+        select concat(member_id,club_id)  from zhihu_rbac_role_member group by concat(member_id,club_id)  having count(*)>1
+    ) 
+)
+
+-- 查询出错 (1093): You can't specify target table 'zhihu_rbac_role_member' for update in FROM clause
+
+
+
