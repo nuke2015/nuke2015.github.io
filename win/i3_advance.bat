c1a93d40f4be70608bd962b4eeacc5f14ca15f7d
nuke2015 <196971567@qq.com>
2020-01-13 08:59:21 +0800

    +

--- win/i3_advance.bat
+++ win/i3_advance.bat
@@ -1,10 +1,12 @@
 ﻿rem 一键安装java,ant,android,nodejs
 
 set ROOT_PATH=%CD%
-set JAVA_HOME=%ROOT_PATH%\Java\jdk
-set ANT_HOME=%ROOT_PATH%\ant
+set REDIS_HOME=%CD%\redis
+set JAVA_HOME=%ROOT_PATH%\Java\jdk1.7.0_45
+set ANT_HOME=%ROOT_PATH%\apache-ant-1.9.4
+set PHP_HOME=%ROOT_PATH%\php-cli
+set GIT_HOME=%ROOT_PATH%\Git
 set NODE_HOME=%ROOT_PATH%\Nodejs
-set ANDROID_HOME=%ROOT_PATH%\Nodejs
 set ANDROID_HOME=%ROOT_PATH%\ANDROID_HOME
 set GIT_HOME=%ROOT_PATH%\Git
 set PHP_HOME=%ROOT_PATH%\php\php
@@ -16,10 +18,11 @@ set PHANTOMJS=%ROOT_PATH%\phantomjs
 set SVN_PATH=%ROOT_PATH%\Apache-Subversion
 
 setx JAVA_HOME %JAVA_HOME%
+setx GRADLE_HOME %ROOT_PATH%\gradle
 setx ANT_HOME %ANT_HOME%
 setx GOROOT %GOROOT%
 setx GOPATH %GOPATH%
 
-setx path %ROOT_PATH%\sbin;%PHANTOMJS%\bin;%GOPATH%\bin;%GOROOT%\bin;%PHP_HOME%;%GIT_HOME%\bin;%NODE_HOME%;%NODE_HOME%\node_modules\.bin;%ANT_HOME%\bin;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%JAVA_HOME%\bin;%FASTBOOT_PATH%;%CHROME_PATH%;%SVN_PATH%\bin;%path%;
+setx path %ROOT_PATH%\sbin;%GRADLE_HOME\bin%;%REDIS_HOME%;%PHANTOMJS%\bin;%GOPATH%\bin;%GOROOT%\bin;%PHP_HOME%;%GIT_HOME%\bin;%NODE_HOME%;%NODE_HOME%\node_modules\.bin;%ANT_HOME%\bin;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%JAVA_HOME%\bin;%FASTBOOT_PATH%;%CHROME_PATH%;%SVN_PATH%\bin;%path%;
 
 pause
