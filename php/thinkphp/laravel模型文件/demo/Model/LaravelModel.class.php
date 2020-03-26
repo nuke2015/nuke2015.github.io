5517d8612ed3761b915c09dd9ef1524817c967fe
nuke2015 <196971567@qq.com>
2020-03-26 08:56:54 +0800

    修复,laraval,最原始的装载.

--- php/thinkphp/laravel模型文件/demo/Model/LaravelModel.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Laravel Model
- */
-import("@.ORG.Laravel.eloquent");
-abstract class LaravelModel extends eloquent
-{
-}
--- php/thinkphp/laravel模型文件/eloquent.class.php
+++ php/thinkphp/laravel模型文件/eloquent.class.php
@@ -1,24 +1,21 @@
 <?php
-
 //加载功能类库
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-// Autoload 自动载入
-require_once __DIR__ . '/vendor/autoload.php';
-
-class eloquent extends Illuminate\Database\Eloquent\Model
+class Myeloquent extends Illuminate\Database\Eloquent\Model
 {
     public static $capsule = null;
-    public function __construct() {
+    public function __construct()
+    {
         if (!self::$capsule) {
-            
+
             // Eloquent ORM
             $capsule = new Capsule;
-            
+
             //配置文件
-            $db['eloquent'] = ['driver' => 'mysql', 'host' => 'localhost', 'database' => 'testdb', 'username' => 'root', 'password' => '', 'charset' => 'utf8', 'collation' => 'utf8_general_ci', 'prefix' => ''];
-            
-            $capsule->addConnection($db['eloquent']);
+            $conf = ['driver' => 'mysql', 'host' => 'localhost', 'database' => 'test', 'username' => 'root', 'password' => 'root', 'charset' => 'utf8', 'collation' => 'utf8_general_ci', 'prefix' => ''];
+
+            $capsule->addConnection($conf);
             $capsule->bootEloquent();
             self::$capsule = $capsule;
         }
