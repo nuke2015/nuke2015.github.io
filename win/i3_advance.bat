dde162f42e25160cc8d9016804b1ad0cb46c9f4e
nuke2015 <196971567@qq.com>
2020-03-29 10:46:40 +0800

    +

--- win/i3_advance.bat
+++ win/i3_advance.bat
@@ -4,16 +4,13 @@ set ROOT_PATH=%CD%
 set JAVA_HOME=%ROOT_PATH%\Java\jdk
 set ANT_HOME=%ROOT_PATH%\ant
 set NODE_HOME=%ROOT_PATH%\Nodejs
-set ANDROID_HOME=%ROOT_PATH%\Nodejs
 set ANDROID_HOME=%ROOT_PATH%\ANDROID_HOME
 set GIT_HOME=%ROOT_PATH%\Git
 set PHP_HOME=%ROOT_PATH%\php\php
 set GOROOT=%ROOT_PATH%\go
-set GOPATH=D:\gopath
 set FASTBOOT_PATH=%ROOT_PATH%\fastboot\
-set CHROME_PATH=D:\soft\chrome\
-set PHANTOMJS=%ROOT_PATH%\phantomjs
-set SVN_PATH=%ROOT_PATH%\Apache-Subversion
+set GOPATH=D:\gopath
+set CHROME_PATH=D:\working\chrome\
 set COMPOSER_PATH=D:\git\composer\vendor\
 
 setx JAVA_HOME %JAVA_HOME%
@@ -21,6 +18,6 @@ setx ANT_HOME %ANT_HOME%
 setx GOROOT %GOROOT%
 setx GOPATH %GOPATH%
 
-setx path %COMPOSER_PATH%\bin;%ROOT_PATH%\sbin;%PHANTOMJS%\bin;%GOPATH%\bin;%GOROOT%\bin;%PHP_HOME%;%GIT_HOME%\bin;%NODE_HOME%;%NODE_HOME%\node_modules\.bin;%ANT_HOME%\bin;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%JAVA_HOME%\bin;%FASTBOOT_PATH%;%CHROME_PATH%;%SVN_PATH%\bin;%path%;
+setx path %COMPOSER_PATH%\bin;%ROOT_PATH%\sbin;%PHANTOMJS%\bin;%GOPATH%\bin;%GOROOT%\bin;%PHP_HOME%;%GIT_HOME%\bin;%NODE_HOME%;%NODE_HOME%\node_modules\.bin;%ANT_HOME%\bin;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%JAVA_HOME%\bin;%FASTBOOT_PATH%;%CHROME_PATH%;%path%;
 
 pause
