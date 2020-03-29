6b802fe15ed2156e8596f6365e3e8160288c829f
nuke2015 <196971567@qq.com>
2020-03-29 10:41:50 +0800

    +phpunit

--- win/i3_advance.bat
+++ win/i3_advance.bat
@@ -14,12 +14,13 @@ set FASTBOOT_PATH=%ROOT_PATH%\fastboot\
 set CHROME_PATH=D:\soft\chrome\
 set PHANTOMJS=%ROOT_PATH%\phantomjs
 set SVN_PATH=%ROOT_PATH%\Apache-Subversion
+set COMPOSER_PATH=D:\git\composer\vendor\
 
 setx JAVA_HOME %JAVA_HOME%
 setx ANT_HOME %ANT_HOME%
 setx GOROOT %GOROOT%
 setx GOPATH %GOPATH%
 
-setx path %ROOT_PATH%\sbin;%PHANTOMJS%\bin;%GOPATH%\bin;%GOROOT%\bin;%PHP_HOME%;%GIT_HOME%\bin;%NODE_HOME%;%NODE_HOME%\node_modules\.bin;%ANT_HOME%\bin;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%JAVA_HOME%\bin;%FASTBOOT_PATH%;%CHROME_PATH%;%SVN_PATH%\bin;%path%;
+setx path %COMPOSER_PATH%\bin;%ROOT_PATH%\sbin;%PHANTOMJS%\bin;%GOPATH%\bin;%GOROOT%\bin;%PHP_HOME%;%GIT_HOME%\bin;%NODE_HOME%;%NODE_HOME%\node_modules\.bin;%ANT_HOME%\bin;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%JAVA_HOME%\bin;%FASTBOOT_PATH%;%CHROME_PATH%;%SVN_PATH%\bin;%path%;
 
 pause
