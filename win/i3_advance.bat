7ed6ecf7a066acd00b96ce1d43ca6a5e9129b5f3
nuke2015 <196971567@qq.com>
2020-04-07 09:55:22 +0800

    +go proxy

--- win/i3_advance.bat
+++ win/i3_advance.bat
@@ -20,6 +20,10 @@ setx ANT_HOME %ANT_HOME%
 setx GOROOT %GOROOT%
 setx GOPATH %GOPATH%
 
+rem goproxy
+setx export GOPROXY="https://mirrors.aliyun.com/goproxy/"
+setx export GO111MODULE=on 
+
 setx path %CYGWIN_HOME%\bin;%MINGW_HOME%\bin;%COMPOSER_PATH%\bin;%ROOT_PATH%\sbin;%PHANTOMJS%\bin;%GOPATH%\bin;%GOROOT%\bin;%PHP_HOME%;%GIT_HOME%\bin;%NODE_HOME%;%NODE_HOME%\node_modules\.bin;%ANT_HOME%\bin;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%JAVA_HOME%\bin;%FASTBOOT_PATH%;%CHROME_PATH%;%path%;
 
 pause
