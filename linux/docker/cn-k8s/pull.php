9941e8ac76d5cf5f27927c6b74c325d3aeb7fc6e
fengfeng <it07@qq.com>
2020-04-12 14:50:14 +0800

    天朝手动拉镜像

---
+++ linux/docker/cn-k8s/pull.php
@@ -0,0 +1,30 @@
+<?php
+
+$a=file('./image-list.txt');
+
+$list=[];
+if($a && count($a)){
+    foreach ($a as $key => $value) {
+        $value=trim($value);
+        if($value){
+            $list[]=$value;
+        }
+    }
+}
+
+foreach ($list as $key => $value) {
+    echo "dk pull $value \r\n";
+}
+
+echo "\r\n";
+echo "\r\n";
+foreach ($list as $key => $value) {
+    echo "dk save $value >".md5($value).".tar\r\n";
+}
+
+echo "\r\n";
+echo "\r\n";
+foreach ($list as $key => $value) {
+    echo "dk load ".md5($value).".tar>$value\r\n";
+}
+
