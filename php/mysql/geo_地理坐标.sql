5172097e60e6977c323fa5f787a3dd929260aa10
nuke2015 <196971567@qq.com>
2020-08-03 11:46:12 +0800

    ++++

--- php/mysql/geo_地理坐标.sql
+++ php/mysql/geo_地理坐标.sql
@@ -23,3 +23,20 @@ update ddys_corp_config
 set geoaddr=POINT(114,82)
 where corp_id=2
 
+
+
+DROP TABLE IF EXISTS `ddys_corp_geo`;
+CREATE TABLE `ddys_corp_geo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `geoaddr` geometry NOT NULL COMMENT '坐标(经纬度)',
+  `create_at` int(11) NOT NULL COMMENT '创建时间',
+  `corp_id` int(11) NOT NULL COMMENT '加盟商id',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unq_uid` (`id`),
+  SPATIAL KEY `spa_geo` (`geoaddr`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='商户门店物理地址-经纬度';
+
+
+
+
+
