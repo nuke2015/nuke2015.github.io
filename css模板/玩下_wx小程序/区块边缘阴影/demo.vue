e61fd3252f49766f24538aaa477b99e6d858570e
nuke2015 <196971567@qq.com>
2020-09-26 10:01:38 +0800

    ++

---
+++ css模板/玩下_wx小程序/区块边缘阴影/demo.vue
@@ -0,0 +1,19 @@
+
+<view class="xueye">
+                111
+            </view>
+.xueye {
+        width: 100%;
+        height: 80rpx;
+        float: center;
+        text-align: center;
+        padding: 30px 0 45px 0;
+        font-size: 23px;
+        text-align: center;
+        border: 5px solid #edf0f7;
+        border-top: unset;
+        border-bottom-right-radius: 15px;
+        border-bottom-left-radius: 15px;
+        box-shadow: 0px 4px 8px 0px rgba(221, 221, 221, 0.4);
+        background: #fff;
+    }
---
+++ css模板/玩下_wx小程序/圆形按钮/demo.vue
@@ -0,0 +1,17 @@
+
+<view class="xueye">
+                111
+            </view>
+.xueye{
+        width:100%;
+        height:80rpx;
+        float: center;
+        text-align:center;
+        font-size: 60rpx;
+        color:#0BB20C;
+        background-color: #7916be;
+        flex-direction: column;
+        box-shadow: 0px 4px 8px 0px rgba(221, 221, 221, 0.4);
+        border:1px solid red;
+        border-radius: 100rpx;
+    }
