301faacbb2542db627bda0df2a3f9ddd08c21dbf
nuke2015 <196971567@qq.com>
2021-09-21 17:06:39 +0800

    +++

--- linux/android/MI8_fk_more.bat
+++ linux/android/MI8_fk_more.bat
@@ -1,49 +1,58 @@
 
 
-#原生输入法
+rem 原生输入法
 adb shell pm uninstall --user 0 com.sohu.inputmethod.sogou.xiaomi
 
-#查找手机
-adb shell pm uninstall --user 0 com.xiaomi.finddevice
-
-#小爱同学
+rem 小爱同学
 adb shell pm uninstall --user 0 com.miui.voiceassist
 
-#应用扩展
-adb shell pm uninstall --user 0 com.miui.contentcatcher
+rem 应用扩展
+rem adb shell pm uninstall --user 0 com.miui.contentcatcher
 
-#小米互联
+rem 小米互联
 adb shell pm uninstall --user 0 com.xiaomi.mi_connect_service
 
-#排序
+rem 排序
 adb shell pm uninstall --user 0 com.tencent.sorter.sorterserver
 
-#收音机
+rem 收音机
 adb shell pm uninstall --user 0 com.miui.fmservice
 
-#系统更新 
+rem 系统更新 
 adb shell pm uninstall --user 0 com.android.updater
 
-#小米商城
-adb shell pm uninstall --user 0 com.xiaomi.ab
+rem 小米商城
+adb shell pm uninstall --user 0 com.miui.guardprovider
+adb shell pm uninstall --user 0 com.android.provision
+adb shell pm uninstall --user 0 com.miui.miservice
+adb shell pm uninstall --user 0 com.mi.health
+adb shell pm uninstall --user 0 com.mipay.wallet
+adb shell pm uninstall --user 0 com.android.quicksearchbox
+adb shell pm uninstall --user 0 com.miui.personalassistant
+adb shell pm uninstall --user 0 com.qiyi.vedio
+adb shell pm uninstall --user 0 com.baidu.haokan
+adb shell pm uninstall --user 0 com.android.bluetooth
+adb shell pm uninstall --user 0 com.miui.yellowpage
+adb shell pm uninstall --user 0 com.miui.fm
+adb shell pm uninstall --user 0 com.mfashiongallery.emag
 
-#指纹测试
+rem 指纹测试
 adb shell pm uninstall --user 0 com.goodix.gftest
 
-#智慧生活
+rem 智慧生活
 adb shell pm uninstall --user 0 com.miui.hybrid.accessory
 
-#测试
+rem 测试
 adb shell pm uninstall --user 0 com.fingerprints.sensortesttool
 
-#存储号码
+rem 存储号码
 adb shell pm uninstall --user 0 com.android.porviders.blockednumber
 
-#工模测试
+rem 工模测试
 adb shell pm uninstall --user 0 com.longcheertel.cit
 adb shell pm uninstall --user 0 com.longcheertel.AutoTest
 
-#ana
+rem ana
 adb shell pm uninstall --user 0 com.miui.analytics
 
 
