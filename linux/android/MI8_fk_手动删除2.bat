dc57a16dd7612a79d06ea4a974709162adea469e
nuke2015 <196971567@qq.com>
2021-09-22 15:40:48 +0800

    +

--- linux/android/MI8_fk_手动删除2.bat
+++ linux/android/MI8_fk_手动删除2.bat
@@ -1,228 +1,228 @@
 
-#壁纸
+rem 壁纸
 adb shell pm uninstall --user 0 com.android.wallpapercropper
 
-#下载
-#adb shell pm uninstall --user 0 com.android.providers.downloads.ui
+rem 下载
+rem adb shell pm uninstall --user 0 com.android.providers.downloads.ui
 
-#性能
+rem 性能
 adb shell pm uninstall --user 0 com.qualcomm.qti.performancemode
 
-#证书
-#adb shell pm uninstall --user 0 com.android.certinstaller
+rem 证书
+rem adb shell pm uninstall --user 0 com.android.certinstaller
 
-#stk
+rem stk
 adb shell pm uninstall --user 0 com.android.stk
 
-#屏保
+rem 屏保
 adb shell pm uninstall --user 0 com.android.dreams.basic
 
-#系统打印
+rem 系统打印
 adb shell pm uninstall --user 0 com.android.bips
 
-#小米支付
+rem 小米支付
 adb shell pm uninstall --user 0 com.xiaomi.miplay_client
 
-#智能服务
+rem 智能服务
 adb shell pm uninstall --user 0 com.miui.systemAdSolution
 
-#萬向息屏
+rem 萬向息屏
 adb shell pm uninstall --user 0 com.miui.aod
 
-#音質音效
+rem 音質音效
 adb shell pm uninstall --user 0 com.miui.misound
 
-#小米分享
+rem 小米分享
 adb shell pm uninstall --user 0 com.miui.mishare.connectivity
 
-#自由表单
+rem 自由表单
 adb shell pm uninstall --user 0 com.miui.freeform
 
-#小米云服务
+rem 小米云服务
 adb shell pm uninstall --user 0 com.miui.cloudservice
 
-#安全键盘
+rem 安全键盘
 adb shell pm uninstall --user 0 com.miui.securityinputmethod
 
-#银联
+rem 银联
 adb shell pm uninstall --user 0 com.unionpay.tsmservice.mi
 
-#gx
+rem gx
 adb shell pm uninstall --user 0 com.google.android.marvin.talkback
 
 adb shell pm uninstall --user 0 com.qualcomm.qti.services.systemhelper
 
 adb shell pm uninstall --user 0 com.qualcomm.qti.poweroffalarm
 
-#小米助手
+rem 小米助手
 adb shell pm uninstall --user 0 com.xiaomi.aiasst.vision
 
-#ar
+rem ar
 adb shell pm uninstall --user 0 com.google.ar.core
 
-#ai
+rem ai
 adb shell pm uninstall --user 0 com.xiaomi.aiasst.service
 
-#小米备份
-#adb shell pm uninstall --user 0 com.miui.backup
+rem 小米备份
+rem adb shell pm uninstall --user 0 com.miui.backup
 
-#壁纸 
+rem 壁纸 
 adb shell pm uninstall --user 0 com.miui.miwallpaper
 
-#测试
+rem 测试
 adb shell pm uninstall --user 0 com.miui.cit
 
-#云同步
+rem 云同步
 adb shell pm uninstall --user 0 com.miui.micloudsync
 
-#边角刘海
+rem 边角刘海
 adb shell pm uninstall --user 0 com.android.internal.display.cutout.emulation.corner
 
-#云基础
+rem 云基础
 adb shell pm uninstall --user 0 com.miui.cloudservice.sysbase
 
-#用户收集
+rem 用户收集
 adb shell pm uninstall --user 0 com.miui.daemon
 
 
-#
+rem 
 adb shell pm uninstall --user 0 com.fido.xiaomi.uafclient
 
-#
+rem 
 adb shell pm uninstall --user 0 com.fido.asm
 
 adb shell pm uninstall --user 0 com.mobiletools.systemhelper
 
-#定位
-#adb shell pm uninstall --user 0 com.xiaomi.bsp.gps.nps
+rem 定位
+rem adb shell pm uninstall --user 0 com.xiaomi.bsp.gps.nps
 
-#证书管理 
-#adb shell pm uninstall --user 0 com.wapi.wapicertmanage
+rem 证书管理 
+rem adb shell pm uninstall --user 0 com.wapi.wapicertmanage
 
-#
-#adb shell pm uninstall --user 0 com.android.localtransport
+rem 
+rem adb shell pm uninstall --user 0 com.android.localtransport
 
-#有道 
+rem 有道 
 adb shell pm uninstall --user 0 com.miui.translation.youdao
 
-#金山
+rem 金山
 adb shell pm uninstall --user 0 com.miui.translation.kingsoft
 
 
-#上下文
-#adb shell pm uninstall --user 0 com.miui.contentcatcher
+rem 上下文
+rem adb shell pm uninstall --user 0 com.miui.contentcatcher
 
-#
+rem 
 adb shell pm uninstall --user 0 com.android.dynsystem
 
-#计步
+rem 计步
 adb shell pm uninstall --user 0 com.xiaomi.joyose
 
-#电量
+rem 电量
 adb shell pm uninstall --user 0 com.miui.powerkeeper
 
-#
+rem 
 adb shell pm uninstall --user 0 com.miui.translation.xmcloud
 
-#照片屏幕保護
+rem 照片屏幕保護
 adb shell pm uninstall --user 0 com.android.dreams.phototable
 
-#用戶字典
+rem 用戶字典
 adb shell pm uninstall --user 0 com.android.providers.userdictionary
 
-#桌纸备份
+rem 桌纸备份
 adb shell pm uninstall --user 0 com.android.wallpaperbackup
 
-#融合位置服務
-#adb shell pm uninstall --user 0 com.xiaomi.location.fused
+rem 融合位置服務
+rem adb shell pm uninstall --user 0 com.xiaomi.location.fused
 
-#雙劉海屏
+rem 雙劉海屏
 adb shell pm uninstall --user 0 com.android.internal.display.cutout.emulation.double
 
-#一體化位置信息
-#adb shell pm uninstall --user 0 com.android.location.fused
+rem 一體化位置信息
+rem adb shell pm uninstall --user 0 com.android.location.fused
 
-#工作設置
+rem 工作設置
 adb shell pm uninstall --user 0 com.android.managedprovisioning
 
-#運營商默認應用
+rem 運營商默認應用
 adb shell pm uninstall --user 0 com.android.carrierdefaultapp
 
-#
+rem 
 adb shell pm uninstall --user 0 com.miui.translationservice
 
-#adb shell pm uninstall --user 0 com.android.sharedstoragebackup
+rem adb shell pm uninstall --user 0 com.android.sharedstoragebackup
 
 adb shell pm uninstall --user 0 com.android.onetimeinitializer
 
-#時鐘
+rem 時鐘
 adb shell pm uninstall --user 0 com.android.deskclock
 
-#急救信息
+rem 急救信息
 adb shell pm uninstall --user 0 com.android.emergency
 
 
-#备份确认
-#adb shell pm uninstall --user 0 com.android.backupconfirm
+rem 备份确认
+rem adb shell pm uninstall --user 0 com.android.backupconfirm
 
-#系統跟蹤
+rem 系統跟蹤
 adb shell pm uninstall --user 0 com.android.traceur
 
 
 
-#分享
+rem 分享
 adb shell pm uninstall --user 0 android.ext.shared
 
-#服务
+rem 服务
 adb shell pm uninstall --user 0 android.ext.services
 
 
-#服务反馈
+rem 服务反馈
 adb shell pm uninstall --user 0 com.miui.miservice
 
-#蓝牙
+rem 蓝牙
 adb shell pm uninstall --user 0 com.android.bluetooth
 
-#钱包
+rem 钱包
 adb shell pm uninstall --user 0 com.mipay.wallet
 
-#主题
+rem 主题
 adb shell pm uninstall --user 0 com.android.thememanager
 
-#智能助理
+rem 智能助理
 adb shell pm uninstall --user 0 com.miui.personalassistant
 
-#智能出行
+rem 智能出行
 adb shell pm uninstall --user 0 com.miui.smarttravel
 
-#智能卡
+rem 智能卡
 adb shell pm uninstall --user 0 com.miui.tsmclient
 
-#画报
+rem 画报
 adb shell pm uninstall --user 0 com.mfashiongallery.emag
 
-#搜索
+rem 搜索
 adb shell pm uninstall --user 0 com.android.quicksearchbox
 
-#黄页
+rem 黄页
 adb shell pm uninstall --user 0 com.miui.yellowpage
 
-#售后自动测试
+rem 售后自动测试
 adb shell pm uninstall --user 0 com.longcheertel.AutoTest
 
-#收音机
+rem 收音机
 adb shell pm uninstall --user 0 com.caf.fmradio
 
-#常用语
+rem 常用语
 adb shell pm uninstall --user 0 com.miui.phrase
 
-#蓝牙
+rem 蓝牙
 adb shell pm uninstall --user 0 com.xiaomi.bluetooth
 
-#anylytics
+rem anylytics
 adb shell pm uninstall --user 0 com.miui.analytics
 
-#用户反馈
+rem 用户反馈
 adb shell pm uninstall --user 0 com.miui.bugreport
 
 
