b973567cd4d26ea3d6c72a7e104bff71a8bc2efd
nuke2015 <196971567@qq.com>
2021-10-28 09:50:23 +0800

    +min

---
+++ linux/android/MI8_fk_my.bat
@@ -0,0 +1,15 @@
+
+rem ÊÕÒô»ú
+adb shell pm uninstall --user 0 com.caf.fmradio
+
+adb shell pm uninstall --user 0 com.sohu.inputmethod.sogou.xiaomi
+
+adb shell pm uninstall --user 0 com.miui.miservice
+
+adb shell pm uninstall --user 0 com.android.bluetooth
+
+adb shell pm uninstall --user 0 com.mipay.wallet
+
+
+
+pause
