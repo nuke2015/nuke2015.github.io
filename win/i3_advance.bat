7bd9d59889ff3d6979655f1336e25d87315179cc
nuke2015 <196971567@qq.com>
2022-02-25 17:51:10 +0800

    +

--- win/i3_advance.bat
+++ win/i3_advance.bat
@@ -1,12 +1,18 @@
 rem 一键安装java,ant,android,nodejs
 
-set ROOT_PATH=D:\advance
+set ROOT_PATH=%CD%
 set JAVA_HOME=%ROOT_PATH%\Java\jdk
+set ANT_HOME=%ROOT_PATH%\ant
 set PYTHON_HOME=%ROOT_PATH%\python
+set PYENV_HOME=%ROOT_PATH%\pyenv
 set NODE_HOME=%ROOT_PATH%\Nodejs
+set ANDROID_HOME=%ROOT_PATH%\ANDROID_HOME
 set GIT_HOME=%ROOT_PATH%\Git
 set PHP_HOME=%ROOT_PATH%\php\php
+set MINGW_HOME=%ROOT_PATH%\mingw
+set CYGWIN_HOME=%ROOT_PATH%\cygwin
 set GOROOT=%ROOT_PATH%\go
+set FASTBOOT_PATH=%ROOT_PATH%\fastboot\
 set GOPATH=D:\gopath
 set CHROME_PATH=D:\working\chrome\
 set COMPOSER_PATH=D:\git\composer\vendor\
@@ -21,6 +27,7 @@ rem goproxy
 setx GOPROXY "https://mirrors.aliyun.com/goproxy/"
 setx GO111MODULE on 
 
-setx path "%ROOT_PATH%\cygwin\bin;%ROOT_PATH%\pyenv-win\bin\;%COMPOSER_PATH%\bin;%ROOT_PATH%\sbin;%PHANTOMJS%\bin;%GOPATH%\bin;%GOROOT%\bin;%PHP_HOME%;%GIT_HOME%\bin;%NODE_HOME%;%NODE_HOME%\node_modules\.bin;%ANT_HOME%\bin;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%JAVA_HOME%\bin;%FASTBOOT_PATH%;%CHROME_PATH%;%PYTHON_HOME%;%PYTHON_HOME%\Scripts;%path%";
-@echo %path%;
+setx PATH %PYENV_HOME%\pyenv-win\bin\;%CYGWIN_HOME%\bin;%MINGW_HOME%\bin;%COMPOSER_PATH%\bin;%ROOT_PATH%\sbin;%PHANTOMJS%\bin;%GOPATH%\bin;%GOROOT%\bin;%PHP_HOME%;%GIT_HOME%\bin;%NODE_HOME%;%NODE_HOME%\node_modules\.bin;%ANT_HOME%\bin;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%JAVA_HOME%\bin;%FASTBOOT_PATH%;%CHROME_PATH%;%PYTHON_HOME%;%PYTHON_HOME%/Scripts;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;
+
+
 pause
