a8d78c9b29c64dabe819f0e53b9a744c0666653c
nuke2015 <196971567@qq.com>
2022-04-22 16:10:45 +0800

    +++++

--- html/poster_商品海报/poster_feng.js
+++ html/poster_商品海报/poster_feng.js
@@ -1,7 +1,6 @@
-var c = document.getElementById("myCanvas"); // 使用id来寻找canvas元素
-    
+
     var api = {
-        make: function(c) {
+        make: function(c,img_product,img_qrcode) {
             var cxt = c.getContext("2d"); // 创建context对象
 
             // 设置myCanvas的宽高
@@ -14,7 +13,7 @@ var c = document.getElementById("myCanvas"); // 使用id来寻找canvas元素
 
             // 把图片绘制到myCanvas
             var img = new Image()
-            img.src = "imgdemo.jpg" // 图片路径
+            img.src = img_product // 图片路径
 
             // 为了防止图片还没加载完成就执行drawImage，需要延迟10ms再执行drawImage
             setTimeout(function() {
@@ -24,7 +23,7 @@ var c = document.getElementById("myCanvas"); // 使用id来寻找canvas元素
             // 绘制商品标题，自动换行
             cxt.fillStyle = "#333";
             cxt.font = "15px bold 黑体";
-            var str = "火山泥岩洗面奶男士专用控油祛痘去黑头去油除螨虫学生补水洁面乳【包邮】"
+            var str = "集采平台:【厂家直供】日本进口一级帮宝适拉拉裤箱装大码36X2片"
             cxt.textBaseline = "middle";
             cxt.textAlign = "left";
             var lineWidth = 0;
@@ -61,44 +60,66 @@ var c = document.getElementById("myCanvas"); // 使用id来寻找canvas元素
             cxt.fillText(str_price, 26, 380);
 
             // 绘制券面值
-            cxt.fillStyle = "#fff";
+            cxt.fillStyle = "#52f374f2";
             cxt.font = "normal bold 15px 黑体";
-            var str_quan = "券 ¥9.90"
+            var str_quan = "券 ¥19.90"
             cxt.textBaseline = "middle";
             cxt.textAlign = "left";
             cxt.fillText(str_quan, 230, 380);
 
             // 绘制二维码
             var qrcode = new Image()
-            qrcode.src = "qrcode.png" // 二维码图片路径
+            qrcode.src = img_qrcode // 二维码图片路径
 
             // 为了防止图片还没加载完成就执行drawImage，需要延迟10ms再执行drawImage
             setTimeout(function() {
                 cxt.drawImage(qrcode, 95, 420, 130, 130);
+                base64 = c.toDataURL("image/png");
+                console.log(base64);
+                api.download(base64);
             }, 10)
-              base64 = c.toDataURL("image/png"); 
-              console.log(base64);
-             api.download(base64);
         },
 
         download: function(imgUrl) {
-         // 如果浏览器支持msSaveOrOpenBlob方法（也就是使用IE浏览器的时候），那么调用该方法去下载图片
-          if (window.navigator.msSaveOrOpenBlob) {
-            var bstr = atob(imgUrl.split(',')[1])
-            var n = bstr.length
-            var u8arr = new Uint8Array(n)
-            while (n--) {
-              u8arr[n] = bstr.charCodeAt(n)
+            // 如果浏览器支持msSaveOrOpenBlob方法（也就是使用IE浏览器的时候），那么调用该方法去下载图片
+            if (window.navigator.msSaveOrOpenBlob) {
+                var bstr = atob(imgUrl.split(',')[1])
+                var n = bstr.length
+                var u8arr = new Uint8Array(n)
+                while (n--) {
+                    u8arr[n] = bstr.charCodeAt(n)
+                }
+                var blob = new Blob([u8arr])
+                window.navigator.msSaveOrOpenBlob(blob, 'chart-download' + '.' + 'png')
+            } else {
+                // 这里就按照chrome等新版浏览器来处理
+                const a = document.createElement('a')
+                a.href = imgUrl
+                a.setAttribute('download', 'result')
+                a.click()
             }
-            var blob = new Blob([u8arr])
-            window.navigator.msSaveOrOpenBlob(blob, 'chart-download' + '.' + 'png')
-          } else {
-            // 这里就按照chrome等新版浏览器来处理
-            const a = document.createElement('a')
-            a.href = imgUrl
-            a.setAttribute('download', 'chart-download')
-            a.click()
-          }
-        }
+        },
+        getBase64: function(url, callback) {
+            var Img = new Image(),
+                dataURL = '';
+            Img.src = url + '?v=' + Math.random();
+            Img.setAttribute('crossOrigin', 'Anonymous');
+            Img.onload = function() {
+                var canvas = document.createElement('canvas'),
+                    width = Img.width,
+                    height = Img.height;
+                canvas.width = width;
+                canvas.height = height;
+                canvas.getContext('2d').drawImage(Img, 0, 0, width, height);
+                dataURL = canvas.toDataURL('image/jpeg');
+                return callback ? callback(dataURL) : null;
+            };
+        },
     }
-    api.make(c);
+    var c = document.getElementById("myCanvas"); // 使用id来寻找canvas元素
+    var pic='https://upload.jjys168.com/jiajiamuying_upload/20220419/1650357863625e76670b28d.png@150h_150w_1e_1c';
+    api.getBase64(pic,function(r1){
+        console.log(pic,r1);
+        api.make(c,r1,r1);
+    });
+    // api.make(c,'imgdemo.jpg','qrcode.png');
